19283e641962502cf5e5ba3e5933f6a31814a16f
Simon Simeonov <simeon.p.simeonov92@gmail.com>
2023-09-06 11:14:44 +0300

    css tweaks, finished with JS

--- script.js
+++ script.js
@@ -6,15 +6,53 @@ const details = document.querySelector('.details');
 
 search.addEventListener('click', () => {
     const APIKey = '15c07f3169d2ba456da4d3d4b7bdc418';
-    const city = document.querySelector('search-box input').value;
+    const city = document.querySelector('.search-box input').value;
 
     if (city === '') return;
 
-    fetch().then(response => response.json()).then(json => {
-        if (json.code === '404') {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => {
+        if (json.cod === '404') {
             container.style.height = '400px';
-            
+            weather.style.display = 'none';
+            details.style.display = 'none';
+            notFound.style.display = 'block';
+            notFound.classList.add('fadeIn');
+            return;
+        }
+
+        notFound.style.display = 'none';
+        notFound.classList.remove('fadeIn');
+
+        const weatherImage = document.querySelector('.weatherimg');
+        const temperature = document.querySelector('.temp');
+        const description = document.querySelector('.desc');
+        const wind = document.querySelector('.windspeed span');
+        const humidity = document.querySelector('.humidity span');
+
+        temperature.innerText = `${json.main.temp} °C`;
+        description.innerText = `${json.weather[0].main}`;
+        humidity.innerText = `${json.main.humidity}%`;
+        wind.innerText = `${json.wind.speed} m/s`;
+
+        weather.classList.add('fadeIn');
+        container.style.height = '600px';
+
+        switch (json.weather[0].main) {
+            case 'Clear':
+                weatherImage.src = 'images/clear.png';
+                break;
+            case 'Clouds':
+                weatherImage.src = 'images/cloud.png';
+                break;
+            case 'Mist':
+                weatherImage.src = 'images/mist.png';
+                break;
+            case 'Rain':
+                weatherImage.src = 'images/rain.png';
+                break;
+            case 'Snow':
+                weatherImage.src = 'images/snow.png';
+                break;
         }
     })
-
 });
